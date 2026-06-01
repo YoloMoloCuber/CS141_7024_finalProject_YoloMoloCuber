738cd1718d3a17932dbde6eb61bf39076483cfa8
YoloMoloCuber <yolomolocuber215@hotmail.com>
2026-05-31 17:50:17 -0700

    Made progress on main menu

--- src/main/java/OSCN.java
+++ src/main/java/OSCN.java
@@ -20,14 +20,54 @@ public class OSCN extends Application{
   public static Group menu = new Group();
   public static final Color DEFAULT_COLOR = Color.BLACK;
   public static final double DEFAULT_WIDTH = 1.0;
+
+  // initializes the threats at the start for easier processing later.
+  Brown brown = new Brown(4, 0);
+  Blue blue = new Blue(6, 2);
+  Yellow yellow = new Yellow(1, 5);
+  Red red = new Red(8, 2);
+
+  Threat[] threats = {brown, blue, yellow, red};
+
   public void start(Stage stage){
-      Text test = text("If this is visible, it worked!", 700, 450, menu);
+      // Creates the custom colors used for the threats.
+      Color customBrown = Color.web("6e4d10");
+      Color customBlue = Color.web("3335ab");
+      Color customYellow = Color.web("e6eb6e");
+      Color customRed = Color.web("962626");
+
+      // Draws the icons for the threats. Placeholders for now
+      Rectangle brownIcon = drawRect(50, 50, 100, 100, customBrown, menu);
+      Rectangle blueIcon = drawRect(200, 50, 100, 100, customBlue, menu);
+      Rectangle yellowIcon = drawRect(350, 50, 100, 100, customYellow, menu);
+      Rectangle redIcon = drawRect(500, 50, 100, 100, customRed, menu);
+
+      // Important variable for selecting threats and altering them later
+      int selectedIndex = -1;
+
+      // The text that is to show for character descriptions.
+      Text desc = text("Hover over the icons above for character mechanics!", 50, 200, menu);
+      desc.setFont(Font.font("Courier New", FontWeight.NORMAL, 20));
 
       stage.setTitle("Oversimplified Custom Night");
       Scene scene = new Scene(menu, 1600, 900);
       scene.setFill(Color.ALICEBLUE);
       stage.setScene(scene);
       stage.show();
+
+      brownIcon.setOnMouseEntered(e -> {
+        desc.setText(brown.getDescription() + "\n\nCurrent AI Level: " + brown.getDifficulty());
+
+      });
+      blueIcon.setOnMouseEntered(e -> {
+        desc.setText(blue.getDescription() + "\n\nCurrent AI Level: " + blue.getDifficulty());
+      });
+      yellowIcon.setOnMouseEntered(e -> {
+        desc.setText(yellow.getDescription() + "\n\nCurrent AI Level: " + yellow.getDifficulty());
+      });
+      redIcon.setOnMouseEntered(e -> {
+        desc.setText(red.getDescription() + "\n\nCurrent AI Level: " + red.getDifficulty());
+      });
   }
 
   public static Rectangle drawRect(int x, int y, int w, int h, Group group){
@@ -45,7 +85,6 @@ public class OSCN extends Application{
   public static Circle drawCircle(int x, int y, int r, Group group){
       return drawCircle(x, y, r, DEFAULT_COLOR, group);
   }
-
   public static Circle drawCircle(int x, int y, int r, Color c, Group group){
       Circle circle = new Circle(x, y, r);
       circle.setFill(c);
@@ -62,6 +101,112 @@ public class OSCN extends Application{
         text.setFocusTraversable(true);
         group.getChildren().add(text);
         return text;
+  }
+}
+
+/**
+ * Generic information for all the Threats:
+ *
+ * difficulty refers to their AI levels, used in determining when they will move
+ * every movement opportinity.
+ *
+ * location is their current location on the map.
+ *
+ * path is the movement paths they will take, if at all.
+ *
+ * description is the description for their behavior that will show in the main
+ * menu.
+ */
+class Threat{
+  public final int THREAT_INDEX;
+  protected int difficulty;
+  protected int location;
+  protected int[][] path = new int[0][0];
+  protected String description;
+
+  public Threat(int d, int l, int i) {
+      if (d < 0) {
+        d = 0; // Sets the difficulty variable to 0 if the input is lower.
+      } else if (d > 20) {
+        d = 20; // Sets the difficulty variable to 20 if the input is higher.
+      }
+      if (i < 0) {
+        throw new IllegalArgumentException("Index cannot be negative!");
+      }
+      difficulty = d;
+      location = l;
+      THREAT_INDEX = i;
+  }
+  public Threat() {
+      this (0, 0, 0);
+  }
+
+  public int getDifficulty() { // Returns the AI levels of the threat
+    return difficulty;
+  }
+  public String getDescription() { // Returns the description text.
+    return description;
+  }
+}
+
+class Brown extends Threat{ // Code for Brown/Freddy
+
+  public Brown(int d, int l) {
+      super(d, l, 0);
+      description = "Placeholder for Brown";
+  }
+  public Brown() {
+      this(0, 0);
+  }
+
+  public String getDescription() { // Returns the description text.
+    return description;
+  }
+}
+
+class Blue extends Threat{ // Code for Blue/Bonnie
+
+  public Blue(int d, int l) {
+      super(d, l, 1);
+      description = "Placeholder for Blue";
+  }
+  public Blue() {
+      this(0, 0);
+  }
+
+  public String getDescription() { // Returns the description text.
+    return description;
+  }
+}
+
+class Yellow extends Threat{ // Code for Yellow/Chica
+
+  public Yellow(int d, int l) {
+      super(d, l, 2);
+      description = "Placeholder for Yellow";
+
+  }
+  public Yellow() {
+      this(0, 0);
+  }
+
+  public String getDescription() { // Returns the description text.
+    return description;
+  }
+}
+
+class Red extends Threat{ // Code for Red/Foxy
+
+  public Red(int d, int l) {
+      super(d, l, 3);
+      description = "Placeholder for Red";
+  }
+  public Red() {
+      this(0, 0);
+  }
+
+  public String getDescription() { // Returns the description text.
+    return description;
   }
 }
 
